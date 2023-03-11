09623aaea1d796bce156212ef8f5fdcea7bb9e47
Addison Wanlass <hi@addison.codes>
2023-03-10 21:33:28 -0500

    feat: add oauth support

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,40 @@
-import logo from "./logo.svg";
+import logo from './logo.svg'
 import Widget from './components/Widget'
-
+import { GoogleLogin } from '@react-oauth/google'
 
 function App() {
+  const responseMessage = (response) => {
+    console.log(response)
+  }
+  const errorMessage = (error) => {
+    console.log(error)
+  }
 
   return (
-    <div className="text-center selection:bg-green-900">
-      <header className="flex min-h-screen flex-col items-center bg-[#282c34] text-white">
+    <div className='text-center selection:bg-green-900'>
+      <header className='flex min-h-screen flex-col items-center bg-[#282c34] text-white'>
         <img
           src={logo}
-          className="animate-speed h-24 motion-safe:animate-spin"
-          alt="logo"
+          className='h-24 animate-speed motion-safe:animate-spin'
+          alt='logo'
         />
         <style>
           {
-            "\
+            '\
             .animate-speed{\
               animation-duration:20s;\
             }\
-          "
+          '
           }
         </style>
-        <p className="bg-gradient-to-r from-emerald-300 mb-6 to-sky-300 bg-clip-text text-5xl leading-relaxed font-black text-transparent selection:bg-transparent">
+        <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
+        <p className='mb-6 text-5xl font-black leading-relaxed text-transparent bg-gradient-to-r from-emerald-300 to-sky-300 bg-clip-text selection:bg-transparent'>
           Reviews Widgets
         </p>
         <Widget />
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { GoogleOAuthProvider } from '@react-oauth/google'
+import './index.css'
+import App from './App'
+
+const googleClientId = process.env.GOOGLE_OAUTH_CLIENT_ID
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+  <GoogleOAuthProvider clientId={googleClientId}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </GoogleOAuthProvider>,
+  document.getElementById('root')
+)
