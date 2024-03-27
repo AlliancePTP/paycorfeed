48c556343c746242998b2d0e3809f1fa8979663d
Addison Wanlass <hi@addison.codes>
2024-03-27 12:12:58 -0400

    fix: add routing to paycor page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Widget from './components/Widget'
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 import JobPage from './components/JobPage'
+import Paycor from './components/Paycor'
 import { useState } from 'react'
 import { getJobs } from './lib/jobs'
 import { useEffect } from 'react'
@@ -19,6 +20,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Widget jobs={jobs} />} />
           <Route path='job/:id' element={<JobPage jobs={jobs} />} />
+          <Route path='/paycor' element={<Paycor />} />
         </Routes>
       </Router>
     )
