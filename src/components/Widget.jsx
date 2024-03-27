a934aadd2424f3a62eaa390b384b8a1320689a4b
Addison Wanlass <hi@addison.codes>
2024-03-26 21:08:29 -0400

    fix: prevent search form submission

--- src/components/Widget.jsx
+++ src/components/Widget.jsx
@@ -7,28 +7,28 @@ const Widget = ({ jobs }) => {
   const [search, setSearch] = useState('')
 
   useEffect(() => {
-      if (localStorage.getItem('selectedType') != 'null') {
-    setSelectedType(localStorage.getItem('selectedType'))
-  } else {
-    setSelectedType(null)
-  }
+    if (localStorage.getItem('selectedType') != 'null') {
+      setSelectedType(localStorage.getItem('selectedType'))
+    } else {
+      setSelectedType(null)
+    }
   }, [])
 
   return (
-    <div className='container w-screen mx-auto md:w-full'>
+    <div className='container mx-auto w-screen md:w-full'>
       {/* Search */}
       <div className='mx-auto mb-8 w-full p-6 md:w-[46rem]'>
-        <form>
+        <form onSubmit={(e) => e.preventDefault()}>
           <label
             htmlFor='default-search'
-            className='mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white'
+            className='sr-only mb-2 text-sm font-medium text-gray-900 dark:text-white'
           >
             Search
           </label>
           <div className='relative'>
-            <div className='absolute inset-y-0 flex items-center pointer-events-none start-0 ps-3'>
+            <div className='start-0 ps-3 pointer-events-none absolute inset-y-0 flex items-center'>
               <svg
-                className='w-4 h-4 ml-4 text-gray-500 '
+                className='ml-4 h-4 w-4 text-gray-500 '
                 aria-hidden='true'
                 xmlns='http://www.w3.org/2000/svg'
                 fill='none'
@@ -46,10 +46,11 @@ const Widget = ({ jobs }) => {
             <input
               type='search'
               id='default-search'
-              className='block w-full p-4 pl-12 text-xl font-semibold text-gray-900 border border-gray-300 rounded-lg ps-10 bg-gray-50 placeholder:text-xl placeholder:font-semibold focus:border-blue-500 focus:ring-blue-500'
+              className='ps-10 block w-full rounded-lg border border-gray-300 bg-gray-50 p-4 pl-12 text-xl font-semibold text-gray-900 placeholder:text-xl placeholder:font-semibold focus:border-blue-500 focus:ring-blue-500'
               placeholder='Search for jobs'
               required=''
               onChange={(e) => {
+                e.preventDefault()
                 setSearch(e.target.value)
               }}
             />
