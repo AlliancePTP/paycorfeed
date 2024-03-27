606e7bb2e946f43c827290465128c8e35c5f122c
Addison Wanlass <hi@addison.codes>
2024-03-27 12:21:22 -0400

    fix: remove flowbite call and change iframe

--- src/components/Paycor.jsx
+++ src/components/Paycor.jsx
@@ -1,11 +1,15 @@
 const Paycor = () => {
   return (
     <div className='mx-auto flex w-full flex-wrap justify-between p-6 md:w-[46rem] md:flex-nowrap'>
-      <script
-        id='gnewtonjs'
-        type='text/javascript'
-        src='//recruitingbypaycor.com/career/iframe.action?clientId=8a7883d0879c591b0187e3570b4e28cc'
-      ></script>
+      <iframe
+        id='gnewtonIframe'
+        style={{ overflow: 'scroll', height: 4000 }}
+        src='https://recruitingbypaycor.com/career/CareerHome.action?clientId=8a7883d0879c591b0187e3570b4e28cc'
+        width='100%'
+        height='100%'
+        frameBorder={0}
+        scrolling='yes'
+      />
     </div>
   )
 }
