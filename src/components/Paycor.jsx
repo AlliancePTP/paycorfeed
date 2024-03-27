0e8ca321c50b641d82afca5978c92fa7fbba68a2
Addison Wanlass <hi@addison.codes>
2024-03-27 12:27:43 -0400

    chore: test an apply link

--- src/components/Paycor.jsx
+++ src/components/Paycor.jsx
@@ -1,6 +1,9 @@
 const Paycor = () => {
   return (
     <div className='mx-auto flex w-full flex-wrap justify-between p-6 md:w-[46rem] md:flex-nowrap'>
+      <a href='https://recruitingbypaycor.com/career/SubmitResume.action?parentUrl=&clientId=8a7883d0879c591b0187e3570b4e28cc&id=8a7885a88df21562018e3d3982252bd8'>
+        Test Apply Link
+      </a>
       <iframe
         id='gnewtonIframe'
         style={{ overflow: 'scroll', height: 4000 }}
