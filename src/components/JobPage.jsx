1f4f3563a63fd614cd5a78c13aac1ab817d4f5a0
Addison Wanlass <hi@addison.codes>
2024-03-28 00:07:10 -0400

    feat: apply button on each page

--- src/components/JobPage.jsx
+++ src/components/JobPage.jsx
@@ -20,14 +20,25 @@ const Job = ({ jobs }) => {
       >
         Go back
       </button>
-      <h1 className='center text-3xl font-bold'>
+      <h1 className='text-3xl font-bold center'>
         {he.decode(match.title.content)}
       </h1>
-      <div className='mt-6' dangerouslySetInnerHTML={{ __html: description }} />
-      <p>
+      <p className='my-4 font-bold'>
         {match['newton:location'].content}, {match['newton:state'].content}
       </p>
-      <h2>Job ID: {id}</h2>
+      <div className='mt-6' dangerouslySetInnerHTML={{ __html: description }} />
+      <form
+        className='flex justify-center mt-6'
+        method='post'
+        action={`https://recruitingbypaycor.com/career/SubmitResume.action?parentUrl=&clientId=8a7883d0879c591b0187e3570b4e28cc&id=${id}`}
+      >
+        <button
+          className='focus:ring-opacity-7 mb-6 w-48 rounded-md bg-[#0A7CBA] px-4 py-2 font-bold text-white hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-400'
+          type='submit'
+        >
+          Apply Now
+        </button>
+      </form>
     </div>
   )
 }
