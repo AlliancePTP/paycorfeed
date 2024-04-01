37d599cc8b84cf9f7941918c9ab2da926dc66967
hi@addison.codes <hi@addison.codes>
2024-04-01 14:39:28 -0400

    feat: new proxy

--- src/components/Jobs.tsx
+++ src/components/Jobs.tsx
@@ -2,16 +2,16 @@ import he from 'he'
 
 const Job = ({ job }: any) => {
   return (
-    <div className='mx-auto flex w-full flex-wrap justify-between p-6 md:w-[46rem] md:flex-nowrap'>
+    <div className='mx-auto flex flex-wrap justify-between p-6 md:w-[46rem] md:flex-nowrap'>
       <a
         className='w-full font-extrabold text-[#0A7CBA] underline underline-offset-2 md:w-auto'
         href={'job/' + job.id.content}
       >
         {he.decode(job.title.content)}
       </a>
-      <p className='italic font-bold'>
-        {job['newton:location'].content}, {job['newton:state'].content}
-      </p>
+        <p className='italic font-bold grow w-36 md:text-end'>
+          {job['newton:location'].content}, {job['newton:state'].content}
+        </p>
     </div>
   )
 }
--- src/lib/jobs.jsx
+++ src/lib/jobs.jsx
@@ -3,15 +3,21 @@ import { useEffect, useState } from 'react'
 
 export async function getJobs() {
   const url =
-    'https://corsproxy.io/?' +
-    encodeURIComponent(
+    'https://cors-proxy.aptp-account.workers.dev/?' +
       'https://recruitingbypaycor.com/career/CareerAtomFeed.action?clientId=8a7883d0879c591b0187e3570b4e28cc'
-    )
+    
 
   return fetch(url, {
-    cache: 'no-cache'
-    // mode: 'no-cors'
-  })
+    method: 'GET',
+    cache: 'no-cache',
+    headers: {
+          'x-cors-headers': JSON.stringify({
+      // allows to send forbidden headers
+      // https://developer.mozilla.org/en-US/docs/Glossary/Forbidden_header_name
+      'cookies': 'x=123'
+    })
+  } 
+    })
     .then((response) => response.text())
     .then((xml) => {
       // Parse the XML into an array
