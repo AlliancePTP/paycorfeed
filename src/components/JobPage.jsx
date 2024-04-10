52f334dce7860baaccf5d9675416d8c39931cd3e
Addison Wanlass <hi@addison.codes>
2024-04-10 16:58:35 -0400

    fix: go back button always home

--- src/components/JobPage.jsx
+++ src/components/JobPage.jsx
@@ -16,7 +16,7 @@ const Job = ({ jobs }) => {
     <div className='container mx-auto mt-6 flex w-[46rem] flex-col'>
       <button
         className='focus:ring-opacity-7 mb-6 w-24 rounded-md bg-[#0A7CBA] px-4 py-2 font-bold text-white hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-400'
-        onClick={() => navigate(-1)}
+        onClick={() => navigate('/')}
       >
         Go back
       </button>
