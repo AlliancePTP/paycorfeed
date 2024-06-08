fcef3c5a0931ad271923a4a5f824f6ed6bfb981a
hi@addison.codes <hi@addison.codes>
2024-06-07 19:25:54 -0500

    fix: job pages for department linked jobs

--- src/components/Jobs.tsx
+++ src/components/Jobs.tsx
@@ -5,7 +5,7 @@ const Job = ({ job }: any) => {
     <div className='mx-auto flex flex-wrap justify-between p-6 md:w-[46rem] md:flex-nowrap'>
       <a
         className='w-full font-extrabold text-[#0A7CBA] underline underline-offset-2 md:w-auto'
-        href={'job/' + job.id.content}
+        href={'https://paycorfeed.lifeatalliance.com/job/' + job.id.content}
       >
         {he.decode(job.title.content)}
       </a>
